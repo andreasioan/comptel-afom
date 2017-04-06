a066570471811bd94499ca64f6876349099ecb2c
andreasioan <a.j.ioannidis@hotmail.com>
2017-04-06 17:15:50 +1000

    ADD: ChartComponent, DashboardComponent, HeaderComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './controllers/common/header/header.component';
+import { DashboardComponent } from './controllers/dashboard/dashboard.component';
+import { ChartComponent } from './controllers/common/chart/chart.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HeaderComponent,
+		DashboardComponent,
+		ChartComponent
+	],
+	imports: [
+		BrowserModule,
+		FormsModule,
+		HttpModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
+
 export class AppModule { }
---
+++ src/app/controllers/common/chart/chart.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'app-chart',
+    templateUrl: 'chart.component.html'
+})
+
+export class ChartComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
