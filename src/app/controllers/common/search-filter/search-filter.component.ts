41707e07808f2d737ca685107446e777b7cae599
andreasioan <a.j.ioannidis@hotmail.com>
2017-04-21 16:48:45 +1000

    Progress

--- src/app/controllers/common/search-filter/search-filter.component.ts
+++ src/app/controllers/common/search-filter/search-filter.component.ts
@@ -8,7 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class SearchFilterComponent implements OnInit {
+    orderBy: boolean;
+    creationDate: boolean;
+    dueDate: boolean;
+    reset: boolean;
+    searchFilterCollapse: boolean;
+
     constructor() { }
 
-    ngOnInit() { }
+    ngOnInit() {
+        this.setFalse();
+        this.onOrderBy();
+        this.searchFilterCollapse = false;
+    }
+
+    searchFilter() {
+        this.searchFilterCollapse = !this.searchFilterCollapse;
+    }
+
+    setFalse() {
+        this.orderBy = false;
+        this.creationDate = false;
+        this.dueDate = false;
+        this.reset = false;
+    }
+
+    onOrderBy() {
+        this.setFalse();
+        this.orderBy = true;
+    }
+
+    onCreationDate() {
+        this.setFalse();
+        this.creationDate = true;
+    }
+
+    onDueDate() {
+        this.setFalse();
+        this.dueDate = true;
+    }
+
+    onReset() {
+        this.setFalse();
+        this.reset = true;
+    }
+
 }
